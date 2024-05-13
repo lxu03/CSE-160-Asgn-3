17096961f3a371c8005e3d20830ccec2208af069
Liam Xu <58871895+lxu03@users.noreply.github.com>
2024-05-12 23:50:32 -0700

    Update Cube.js

--- Cube.js
+++ Cube.js
@@ -3,7 +3,7 @@ class Cube{
       this.type = "cube";
       this.color = color;
       this.matrix = new Matrix4();
-      this.textureNum = 0;
+      this.textureNum = -1;
     }
   
     render() {
@@ -13,17 +13,17 @@ class Cube{
       gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
       gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
       //front
-      drawTriangle3DUV( [0,0,0, 1,1,0, 1,0,0], [0,0, 1, 1,1,0] )
-      drawTriangle3DUV( [0,0,0, 0,1,0, 1,1,0], [0,0, 0, 1, 1,1] )
+      drawTriangle3DUV( [0,0,0, 1,1,0, 1,0,0], [0,0, -1, 1,-1,0] )
+      drawTriangle3DUV( [0,0,0, 0,1,0, 1,1,0], [0,0, 0, 1, -1,1] )
       //top
-      drawTriangle3DUV( [0,1,0, 1,1,1, 1,1,0], [0,0, -1, -1, -1,0] )
-      drawTriangle3DUV( [0,1,0, 0,1,1, 1,1,1], [0,0, 0, -1, -1,-1] )
+      drawTriangle3DUV( [0,1,0, 1,1,1, 1,1,0], [0,0, 1, -1, 1,0] )
+      drawTriangle3DUV( [0,1,0, 0,1,1, 1,1,1], [0,0, 0, -1, 1,-1] )
       //right side
       drawTriangle3DUV( [1,0,0, 1,1,1, 1,1,0], [0,-1,-1, 0, 0,0] )
       drawTriangle3DUV( [1,0,0, 1,0,1, 1,1,1], [0,-1, -1, -1,-1, 0] )
       //left side
-      drawTriangle3DUV( [0,0,0, 0,0,1, 0,1,1], [0,-1, -1, -1,-1, 0] )
-      drawTriangle3DUV( [0,0,0, 0,1,1, 0,1,0], [0,-1,-1, 0, 0,0] )
+      drawTriangle3DUV( [0,0,0, 0,0,1, 0,1,1], [-1,0, 0, 0,0, 1] )
+      drawTriangle3DUV( [0,0,0, 0,1,1, 0,1,0], [-1,0,0, 1, -1,1] )
       //back
       drawTriangle3DUV( [0,0,1, 0,1,1, 1,1,1], [0,0, 0, 1, 1,1] )
       drawTriangle3DUV( [0,0,1, 1,1,1, 1,0,1], [0,0, 1, 1, 1,0] )
@@ -38,21 +38,31 @@ class Cube{
       gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
       gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
       var allverts = [];
+      var alluvs = [];
       allverts = allverts.concat([0,0,0, 1,1,0, 1,0,0]);
       allverts = allverts.concat([0,0,0, 0,1,0, 1,1,0]);
+      allverts = allverts.concat([0,1,0, 1,1,1, 1,1,0]);
       allverts = allverts.concat([0,1,0, 0,1,1, 1,1,1]);
-      allverts = allverts.concat([0,1,0, 1,1,1, 1,1,0]);
+      allverts = allverts.concat([1,0,0, 1,1,1, 1,1,0]);
       allverts = allverts.concat([1,0,0, 1,0,1, 1,1,1]);
-      allverts = allverts.concat([0,1,0, 1,1,1, 1,1,0]);
       allverts = allverts.concat([0,0,0, 0,0,1, 0,1,1]);
       allverts = allverts.concat([0,0,0, 0,1,1, 0,1,0]);
       allverts = allverts.concat([0,0,1, 0,1,1, 1,1,1]);
       allverts = allverts.concat([0,0,1, 1,1,1, 1,0,1]);
       allverts = allverts.concat([0,0,0, 0,0,1, 1,0,1]);
       allverts = allverts.concat([0,0,0, 1,0,1, 1,0,0]);
-      var alluvs = [];
-      alluvs = alluvs.concat([0,0, 1, 1, 1,0])
+      alluvs = alluvs.concat([0,0, -1, 1,-1,0])
+      alluvs = alluvs.concat([0,0, 0, 1, -1,1])
+      alluvs = alluvs.concat([0,0, 1, -1, 1,0])
+      alluvs = alluvs.concat([0,0, 0, -1, 1,-1])
+      alluvs = alluvs.concat([0,-1,-1, 0, 0,0])
+      alluvs = alluvs.concat([0,-1, -1, -1,-1, 0])
+      alluvs = alluvs.concat([-1,0, 0, 0,0, 1])
+      alluvs = alluvs.concat([-1,0,0, 1, -1,1])
       alluvs = alluvs.concat([0,0, 0, 1, 1,1])
+      alluvs = alluvs.concat([0,0, 1, 1, 1,0])
+      alluvs = alluvs.concat([0,0, 0, -1, -1,-1])
+      alluvs = alluvs.concat([0,0, -1, -1, -1,0])
       drawTriangle3DUV(allverts, alluvs);
     }
 
